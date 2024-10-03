4a8e4dc4a394a82511db135fe31e690a3979c3f7
kirtick28 <kirtick.mm2023cse@sece.ac.in>
2024-10-03 23:18:19 +0530

    Created cart class

---
+++ data/cart-class.js
@@ -0,0 +1,68 @@
+class Cart{
+    cartItems;
+    localStorageKey;
+    constructor(localStorageKey){
+        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [],
+        this.localStorageKey=localStorageKey;
+    }
+    saveToLocal(){
+        localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems));
+    }
+    findItem(productId){
+        let matchingProduct;
+        this.cartItems.forEach(cartItem => {
+            if(cartItem.productId == productId){
+                matchingProduct = cartItem;
+            }
+        })
+        return matchingProduct;
+    }
+    addToCart(productId){
+        const deliveryOptionId = 1;
+        const quantity = Number(document.querySelector(`.js-product-quantity-${productId}`).value);
+        let matchingProduct = this.findItem(productId);
+        if(matchingProduct){
+            matchingProduct.quantity += quantity;
+        }
+        else{
+            this.cartItems.push({
+                productId,
+                quantity,
+                deliveryOptionId
+            });
+        }
+        this.saveToLocal();
+    }
+    calculateCartQuantity(){
+        let quantity = 0;
+        this.cartItems.forEach((cartItem)=>{
+            quantity += cartItem.quantity;
+        })
+        return quantity;
+    }
+    removeFromCart(productId){
+        let ind = -1;
+        this.cartItems.forEach((cartItem,index) =>{
+            if(cartItem.productId == productId){
+                ind = index;
+            }
+        })
+        if(ind !== -1){
+            this.cartItems.splice(ind,1);
+        }
+        this.saveToLocal();
+    }
+    updateCartQuantity(productId, newQuantity){
+        const matchingProduct = this.findItem(productId);
+        matchingProduct.quantity = newQuantity;
+        this.saveToLocal();
+    }
+    updateDeliveryOptionId(productId,deliveryOptionId){
+        const matchingProduct = this.findItem(productId);
+        matchingProduct.deliveryOptionId = deliveryOptionId;
+        this.saveToLocal();
+    }
+}
+
+const cart = new Cart('cart-oop');
+console.log(cart);
