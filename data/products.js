8f39b680dc8f006482ede99ff9db1f77e5bfc51b
kirtick28 <kirtick.mm2023cse@sece.ac.in>
2024-10-04 18:06:19 +0530

    Used inheritance and polymorphism to insert Size Chart

--- data/products.js
+++ data/products.js
@@ -35,6 +35,23 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`
   }
+  extraInfoHtml(){
+    return '';
+  }
+}
+
+class Clothing extends Product{
+  sizeChartLink;
+  
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+  extraInfoHtml(){
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>
+    `;
+  }
 }
 
 export const products = [
@@ -697,5 +714,8 @@ export const products = [
     ]
   }
 ].map((productDetails) =>{
+  if(productDetails.type==='clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
